ab83a36a35516cd916e5d9a0b032f2cf6ad0cd73
kartikey <kartikey@kartikeys-MBP.connectify>
2016-02-23 01:23:55 +0530

    Fixit Linking done

--- app/src/main/java/example/com/fixit/splash/MainActivity.java
+++ app/src/main/java/example/com/fixit/splash/MainActivity.java
@@ -1,38 +1,35 @@
 package example.com.fixit.splash;
 
+import android.content.Intent;
+
 import com.daimajia.androidanimations.library.Techniques;
 import com.viksaa.sssplash.lib.activity.AwesomeSplash;
 import com.viksaa.sssplash.lib.cnst.Flags;
 import com.viksaa.sssplash.lib.model.ConfigSplash;
 
-public class MainActivity extends AwesomeSplash {
+import example.com.fixit.MenuActivity;
+import example.com.fixit.R;
 
+public class MainActivity extends AwesomeSplash {
 /*
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-*/
-
+    }*/
     @Override
     public void initSplash(ConfigSplash configSplash) {
          /* you don't have to override every property */
-
         //Customize Circular Reveal
         configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
         configSplash.setAnimCircularRevealDuration(2000); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
-
         //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
-
         //Customize Logo
         configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
         configSplash.setAnimLogoSplashDuration(2000); //int ms
         configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
-
-
         //Customize Path
         configSplash.setPathSplash(Constants.DROID_LOGO); //set path String
         configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
@@ -42,8 +39,6 @@ public class MainActivity extends AwesomeSplash {
         configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
         configSplash.setAnimPathFillingDuration(3000);
         configSplash.setPathSplashFillColor(R.color.colorPrimaryDark); //path object filling color
-
-
         //Customize Title
         configSplash.setTitleSplash("My Awesome App");
         configSplash.setTitleTextColor(R.color.fillColor);
@@ -51,11 +46,10 @@ public class MainActivity extends AwesomeSplash {
         configSplash.setAnimTitleDuration(3000);
         configSplash.setAnimTitleTechnique(Techniques.FlipInX);
 //        configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
-
     }
-
     @Override
     public void animationsFinished() {
-
+Intent intent=new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
